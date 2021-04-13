8b5a13d6d5fcea400d02b645d1fd39649442d4f5
Manosgou <manolisgouvrikos@gmail.com>
2021-04-13 12:13:46 +0300

    Configure Heroku

--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/manos/pharmancyApp/backend/env/bin/python3
+#!/home/manos/pharmancyApp/backend/backend/env/bin/python
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
