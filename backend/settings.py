54f283fb416a7aa7c5d8fed7a9006563526ec539
Manosgou <manolisgouvrikos@gmail.com>
2021-04-15 13:33:08 +0300

    Use .gitignore and dotenv

--- backend/settings.py
+++ backend/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s8j24_v@&%nipnh6a(ug&g5n#as4__5e763wyraq#1+o*)r3i='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['pharmacyapp-api.herokuapp.com','127.0.0.1']
 
