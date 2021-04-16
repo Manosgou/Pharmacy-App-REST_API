3b73f9b0c29469033a716e004b1e247cd30de172
Manosgou <manolisgouvrikos@gmail.com>
2021-04-16 03:06:42 +0300

    Use signals

--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 
 
@@ -12,3 +14,11 @@ class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     domain = models.CharField(max_length=2,choices=DOMAIN,default=PHARMACIST)
 
+@receiver(post_save, sender=Employee)
+def create_employee_profile(sender, instance, created, **kwargs):
+    if created:
+        Employee.objects.create(user=instance)
+
+@receiver(post_save, sender=Employee)
+def save_employee_profile(sender, instance, **kwargs):
+     instance.employee.save()
