1814a7b215b5765047c6642b82b8251850bc3b14
Manos Gouvrikos <33377311+Manosgou@users.noreply.github.com>
2021-06-15 18:30:57 +0300

    Increase token expiration time

--- backend/settings.py
+++ backend/settings.py
@@ -155,4 +155,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
 
-TOKEN_EXPIRED_AFTER_SECONDS = 160
+TOKEN_EXPIRED_AFTER_SECONDS = 1800
